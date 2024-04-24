52fa72ff4976251c5441cb8871a8f37521b3942c
anjali-ojha17 <anjaliojhazenith@gmail.com>
2024-04-24 19:25:29 +0530

    First Commit

---
+++ collect_imgs.py
@@ -0,0 +1,48 @@
+#Import necessary libraries:
+import os
+import cv2
+import time
+
+#Define constants:
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 3
+dataset_size = 100
+
+#Open the webcam:
+cap = cv2.VideoCapture(0)
+
+#Loop through each class: This loop ensures that a directory is created for each class if it doesn't already exist. 
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    time.sleep(2)  # Wait for 2 seconds after creating the directory
+
+    #Collect data for each class:
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    #Capture images:
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(30)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+     
+        counter += 1
+
+#Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ create_dataset.py
@@ -0,0 +1,56 @@
+#necessary libraries:
+import os
+import pickle
+import mediapipe as mp
+import cv2
+import matplotlib.pyplot as plt
+
+#Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+#Define the data directory and initialize lists for data and labels:
+DATA_DIR = './data'
+data = []
+labels = []
+
+#Loop through each subdirectory (class) in the data directory:
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
+
+        x_ = []
+        y_ = []
+
+        #Read each image, convert it to RGB, and process with MediaPipe Hands:
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_rgb)
+
+        #Extract hand landmarks and normalize the coordinates:
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            #Append the extracted data and label to the lists:
+            data.append(data_aux)
+            labels.append(dir_)
+
+#Save the extracted data and labels into a pickle file:
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
